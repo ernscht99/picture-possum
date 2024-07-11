8254279de948f771fde3da381314982c60facbba
Felix Christ <felix.christ@posteo.de>
2024-07-11 16:13:59 +0200

    added script for making ubuntu 24 releases

---
+++ makerelease.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+topdir=picture-possum_$1-$2_amd64
+rm -r $topdir
+mkdir -p $topdir
+mkdir -p $topdir/usr/bin
+mkdir -p $topdir/DEBIAN
+cp build/src/picture_possum $topdir/usr/bin/picture-possum
+echo "Package: picture-possum
+Version: $1
+Maintainer: Felix Christ <felix.christ@posteo.de>
+Depends: openssl
+Architecture: amd64
+Homepage: https://github.com/ernscht99/picture_possum
+Description: A program for manually, but efficiently, tagging and sorting large numbers of images" \
+	> $topdir/DEBIAN/control
+dpkg --build $topdir
+
